034ffc2265ee911ab2234163e76d79dfe95ceb85
oThinas <thiagomartinsprado@gmail.com>
2023-09-08 14:07:06 -0300

    feat: estilização da barra de status

--- App.tsx
+++ App.tsx
@@ -5,10 +5,15 @@ import { NavigationContainer } from '@react-navigation/native';
 /** Screens */
 import { MainNavigator } from './screens';
 
+/** Components */
+import { StatusBar } from 'react-native';
+
 export default function App() {
   return (
     <NavigationContainer>
       <MainNavigator />
+
+      <StatusBar translucent barStyle='dark-content' />
     </NavigationContainer>
   );
 }
