96e56aac5d3c18f96ff460cd220baff89ac5257f
oThinas <thiagomartinsprado@gmail.com>
2023-09-08 22:40:20 -0300

    chore: remoção do tailwind e nativewind

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,1 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['nativewind/babel'],
-};
+module.exports = { presets: ['module:metro-react-native-babel-preset'] };
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./App.{ts,tsx}', './screens/**/*.{ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
-
