bba612a4bddd3ffbef6cca535df01f1de3cf5fb6
oThinas <thiagomartinsprado@gmail.com>
2023-09-09 22:09:52 -0300

    feat: componente de avatar

--- components/Avatar/Avatar.component.tsx
+++ components/Avatar/Avatar.component.tsx
@@ -2,6 +2,7 @@
 import { useRef } from 'react';
 import { Keyboard, StyleSheet, TextInput, View } from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
+import { Person } from 'phosphor-react-native';
 
 /** Components */
 import { IconButtonComponent, ProgressBarComponent } from '..';
@@ -38,25 +39,29 @@ export function AvatarComponent() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.avatarNameContainer}>
-        <Controller
-          control={control}
-          name='avatarName'
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              ref={textInputRef}
-              onBlur={onSubmit}
-              onChangeText={(value) => onChange(value)}
-              value={value}
-              style={styles.avatarName}
-            />
-          )}
-        />
+      <View style={styles.header}>
+        <View style={styles.avatarNameContainer}>
+          <Controller
+            control={control}
+            name='avatarName'
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                ref={textInputRef}
+                onBlur={onSubmit}
+                onChangeText={(value) => onChange(value)}
+                value={value}
+                style={styles.avatarName}
+              />
+            )}
+          />
 
-        <IconButtonComponent iconName='NotePencil' buttonProps={{ onPress: () => handleFocusInput() }} />
+          <IconButtonComponent iconName='NotePencil' buttonProps={{ onPress: () => handleFocusInput() }} />
+        </View>
+
+        <ProgressBarComponent currentXP={avatar.currentXP} level={avatar.level} nextLevelXP={avatar.nextLevelXP} />
       </View>
 
-      <ProgressBarComponent currentXP={avatar.currentXP} level={avatar.level} nextLevelXP={avatar.nextLevelXP} />
+      <Person size={128} />
     </View>
   );
 }
@@ -67,7 +72,9 @@ const styles = StyleSheet.create({
     width: '100%',
     paddingHorizontal: 32,
     alignItems: 'center',
+    gap: 32,
   },
+  header: { alignItems: 'center' },
   avatarNameContainer: {
     flexDirection: 'row',
     gap: 16,
