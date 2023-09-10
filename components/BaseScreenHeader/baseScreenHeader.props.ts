8dfbb6ffb09aa835b2b9b534995ab58959e9c34b
oThinas <thiagomartinsprado@gmail.com>
2023-09-10 13:13:19 -0300

    fix: tipagem de estilos corrigidos

--- components/BaseScreenHeader/baseScreenHeader.props.ts
+++ components/BaseScreenHeader/baseScreenHeader.props.ts
@@ -3,8 +3,9 @@ import { ReactNode } from 'react';
 
 /** Types */
 import { NavigationProps } from '../../types';
+import { SafeAreaViewProps } from 'react-native-safe-area-context';
 
-export interface IBaseScreenHeaderProps {
+export interface IBaseScreenHeaderProps extends SafeAreaViewProps {
   children?: ReactNode;
   navigationProps: NavigationProps<'MissionsList'> | NavigationProps<'AddMission'>;
 }
--- components/Text/text.props.ts
+++ components/Text/text.props.ts
@@ -2,5 +2,5 @@ import { TextProps } from 'react-native';
 
 export interface ITextProps extends TextProps {
   children: string | JSX.Element;
-  additionalStyles?: Record<string, string | number>;
+  additionalStyles?: TextProps['style'];
 }
