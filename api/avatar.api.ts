26516bb774d1143474e25211bb89666684ea9e2e
oThinas <thiagomartinsprado@gmail.com>
2023-09-10 12:52:31 -0300

    refactor: nome de interface corrigido

--- api/avatar.api.ts
+++ api/avatar.api.ts
@@ -2,13 +2,13 @@
 import { endpoints } from '../core/endpoints';
 import { api } from '../lib/axios';
 
-interface IAvatarResponse {
+interface IGetAvatarResponse {
   name: string;
   level: number;
   exp: number;
   needed: number;
 }
 
-export async function getAvatar(): Promise<IAvatarResponse> {
+export async function getAvatar(): Promise<IGetAvatarResponse> {
   return (await api.get(endpoints.character)).data;
 }
