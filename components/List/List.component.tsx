0a8867a5e6c43b1f251395c93c54bd5b11b9ae83
oThinas <thiagomartinsprado@gmail.com>
2023-09-10 13:28:10 -0300

    feat: comportamento para lista de missões vazia

--- components/List/List.component.tsx
+++ components/List/List.component.tsx
@@ -1,29 +1,48 @@
 /** Core */
 import { FlatList, StyleSheet, View } from 'react-native';
+import { SmileyWink } from 'phosphor-react-native';
 
 /** Hooks */
 import { useAppSelector } from '../../hooks/reduxHooks';
 
 /** Components */
-import { MissionComponent } from '..';
+import { MissionComponent, TextComponent } from '..';
 
 export function ListComponent() {
   const missions = useAppSelector((state) => state.missions);
 
   return (
-    <FlatList
-      style={styles.container}
-      data={missions}
-      keyExtractor={(item) => String(item.id)}
-      renderItem={({ item }) => (
-        <MissionComponent mission={item} key={item.id} />
+    <>
+      {!missions.length ? (
+        <View style={styles.emptyList}>
+          <TextComponent additionalStyles={styles.disclamer}>
+            Todas as missões foram concluídas! Parabéns!
+          </TextComponent>
+
+          <SmileyWink size={128}/>
+        </View>
+      ) : (
+        <FlatList
+          style={styles.container}
+          data={missions}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => (
+            <MissionComponent mission={item} key={item.id} />
+          )}
+          ItemSeparatorComponent={() => <View style={styles.divider} />}
+        />
       )}
-      ItemSeparatorComponent={() => <View style={styles.divider} />}
-    />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  emptyList: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 16,
+  },
+  disclamer: { textAlign: 'center' },
   container: { width: '100%' },
   divider: { height: 8 },
 });
